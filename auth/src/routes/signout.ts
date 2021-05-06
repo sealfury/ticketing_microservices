2f6486d5ac10c72bbce782a60115b218db9f9bb7
William <willspecker@gmail.com>
2021-05-06 22:54:51 +0200

    adds signout logic

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -2,8 +2,10 @@ import express from 'express'
 
 const router = express.Router()
 
-router.get('/api/users/currentuser', (req, res) => {
-  res.send('Hello there')
+router.post('/api/users/signout', (req, res) => {
+  req.session = null
+
+  res.send({})
 })
 
 export { router as signoutRouter }
