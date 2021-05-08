6df96b88713abe099cefb90b2a28140d63af1232
William <willspecker@gmail.com>
2021-05-08 21:55:30 +0200

    configures GKE for cluster and SSR for landing page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,33 @@
-export default () => {
+import axios from 'axios'
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser)
+  // axios.get('/api/users/currentuser')
+
   return <h1>Landing Page</h1>
 }
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    // rendering on server
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.googleCloudBuild/api/users/currentuser',
+      {
+        headers: {
+          Host: 'ticketing.dev',
+        },
+      }
+    )
+
+    return data
+  } else {
+    // rendering in browser
+    const { data } = await axios.get('/api/users/currentuser')
+
+    return data
+  }
+  return {}
+}
+
+export default LandingPage
+
