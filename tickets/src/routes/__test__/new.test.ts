fe3345065bf2168366634076a8c0093d0bfe53e9
William <willspecker@gmail.com>
2021-05-10 16:18:06 +0200

    adds ticket creation functionality to new ticket route

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import { app } from '../../app'
+import { Ticket } from '../../models/ticket'
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
   const response = await request(app).post('/api/tickets').send({})
@@ -59,11 +60,22 @@ it('returns an error if an invalid price is provided', async () => {
 })
 
 it('creates a ticket with valid parameters', async () => {
+  let tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(0)
+
+  const title = 'title'
+
   await request(app)
     .post('/api/tickets')
+    .set('Cookie', global.signin())
     .send({
-      title: 'title',
+      title,
       price: 20,
     })
     .expect(201)
+
+  tickets = await Ticket.find({})
+  expect(tickets.length).toEqual(1)
+  expect(tickets[0].price).toEqual(20)
+  expect(tickets[0].title).toEqual(title)
 })
