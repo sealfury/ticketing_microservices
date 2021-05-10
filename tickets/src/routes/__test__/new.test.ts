bcee2030c069c51010ea370c7b00ac392e92b14d
William <willspecker@gmail.com>
2021-05-10 15:56:19 +0200

    adds validation for ticket title and price

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -2,18 +2,13 @@ import request from 'supertest'
 import { app } from '../../app'
 
 it('has a route handler listening to /api/tickets for post requests', async () => {
-  const response = await request(app)
-    .post('/api/tickets')
-    .send({})
-  
+  const response = await request(app).post('/api/tickets').send({})
+
   expect(response.status).not.toEqual(404)
 })
 
 it('can only be accesses if the user is signed in', async () => {
-  await request(app)
-    .post('/api/tickets')
-    .send({})
-    .expect(401)
+  await request(app).post('/api/tickets').send({}).expect(401)
 })
 
 it('returns a status other than 401 if the user is signed in', async () => {
@@ -21,18 +16,46 @@ it('returns a status other than 401 if the user is signed in', async () => {
     .post('/api/tickets')
     .set('Cookie', global.signin())
     .send({})
-  
+
   expect(response.status).not.toEqual(401)
 })
 
 it('returns an error if an invalid title is provided', async () => {
-  
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: '',
+      price: 10,
+    })
+    .expect(400)
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      price: 10,
+    })
+    .expect(400)
 })
 
 it('returns an error if an invalid price is provided', async () => {
-  
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'title',
+      price: -10,
+    })
+    .expect(400)
+
+  await request(app)
+    .post('/api/tickets')
+    .set('Cookie', global.signin())
+    .send({
+      title: 'title',
+    })
+    .expect(400)
 })
 
-it('creates a ticket with valid parameters', async () => {
-  
-})
+it('creates a ticket with valid parameters', async () => {})
