ed011c3253ecf46d80a26b977fabf8b0692caa4c
William <willspecker@gmail.com>
2021-05-12 23:03:01 +0200

    adds method to check reserved status to ticket model updates order model to export order status enum

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import { Order, OrderStatus } from './order'
 
 interface TicketAttrs {
   title: string
@@ -8,6 +9,7 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
   title: string
   price: number
+  isReserved(): Promise<boolean>
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
@@ -38,6 +40,25 @@ const ticketSchema = new mongoose.Schema(
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket(attrs)
+}
+
+// Run query to look at all orders. Find an order where the ticket
+// is the ticket we just found AND the order status is NOT cancelled.
+// If we find an order it means the ticket IS reserved
+ticketSchema.methods.isReserved = async function () {
+  // this === ticket doc that 'isReserved' was called on
+  const existingOrder = await Order.findOne({
+    ticket: this,
+    status: {
+      $in: [
+        OrderStatus.Created,
+        OrderStatus.AwaitingPayment,
+        OrderStatus.Complete,
+      ],
+    },
+  })
+
+  return !!existingOrder
 }
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema)
--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,15 @@
 import mongoose from 'mongoose'
 import express, { Request, Response } from 'express'
-import { requireAuth, validateRequest } from '@gristix/common'
+import {
+  NotFoundError,
+  requireAuth,
+  validateRequest,
+  OrderStatus,
+  BadRequestError,
+} from '@gristix/common'
 import { body } from 'express-validator'
+import { Ticket } from '../models/ticket'
+import { Order } from '../models/order'
 
 const router = express.Router()
 
@@ -20,6 +28,26 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
+    const { ticketId } = req.body
+
+    // Find the ticket the user is trying to order in the db
+    const ticket = await Ticket.findById(ticketId)
+    if (!ticket) {
+      throw new NotFoundError()
+    }
+
+    // Ensure the ticket is not already reserved
+    const isReserved = await ticket.isReserved()
+    if (isReserved) {
+      throw new BadRequestError('Ticket is already reserved!')
+    }
+
+    // Calculate an expiration date for the order
+
+    // Build the order and save it to the db
+
+    // Publish an event saying an order was created
+
     res.send({})
   }
 )
