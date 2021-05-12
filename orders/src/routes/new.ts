8f6302dccbdd0f9ade804d5042d4cc7863d69e15
William <willspecker@gmail.com>
2021-05-12 22:08:39 +0200

    creates order model outlines new order route

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,27 @@
+import mongoose from 'mongoose'
 import express, { Request, Response } from 'express'
+import { requireAuth, validateRequest } from '@gristix/common'
+import { body } from 'express-validator'
 
 const router = express.Router()
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({})
-})
+// NB route is subtly coupled to mongodb due to validation
+// Remove the custom function to remove this coupling
+
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('TicketId must be provided'),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({})
+  }
+)
 
 export { router as newOrderRouter }
