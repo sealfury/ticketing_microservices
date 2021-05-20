0a00c6deb9c4a03a3a32a3a8e7331c2f9d31924c
William <willspecker@gmail.com>
2021-05-20 19:08:03 +0200

    adds tests for orders publishers

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest'
 import { app } from '../../app'
 import { Ticket } from '../../models/ticket'
 import { Order, OrderStatus } from '../../models/order'
+import { natsWrapper } from '../../nats-wrapper'
 
 it('marks an ordered as cancelled', async () => {
   // Create ticket w/ Ticket model
@@ -33,6 +34,29 @@ it('marks an ordered as cancelled', async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
 })
 
+it('emits an order cancelled event', async () => {
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 20,
+  })
+  await ticket.save()
+
+  const user = global.signin()
+
+  const { body: order } = await request(app)
+    .post('/api/orders')
+    .set('Cookie', user)
+    .send({ ticketId: ticket.id })
+    .expect(201)
+
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set('Cookie', user)
+    .send()
+    .expect(204)
+  
+  expect(natsWrapper.client.publish).toHaveBeenCalled()
+})
+
 it.todo('sad path not authorized')
 it.todo('sad path not found')
-it.todo('emits an order cancelled event')
