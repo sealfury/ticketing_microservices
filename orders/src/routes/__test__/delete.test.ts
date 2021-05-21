5ae09d8115752ed8be77efcb91c36ddbae4966ae
William <willspecker@gmail.com>
2021-05-21 23:40:44 +0200

    updates order routes with version field updates orders routes tests

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose'
 import request from 'supertest'
 import { app } from '../../app'
 import { Ticket } from '../../models/ticket'
@@ -7,6 +8,7 @@ import { natsWrapper } from '../../nats-wrapper'
 it('marks an ordered as cancelled', async () => {
   // Create ticket w/ Ticket model
   const ticket = Ticket.build({
+    id: mongoose.Types.ObjectId().toHexString(),
     title: 'concert',
     price: 20,
   })
@@ -36,6 +38,7 @@ it('marks an ordered as cancelled', async () => {
 
 it('emits an order cancelled event', async () => {
   const ticket = Ticket.build({
+    id: mongoose.Types.ObjectId().toHexString(),
     title: 'concert',
     price: 20,
   })
@@ -54,9 +57,6 @@ it('emits an order cancelled event', async () => {
     .set('Cookie', user)
     .send()
     .expect(204)
-  
+
   expect(natsWrapper.client.publish).toHaveBeenCalled()
 })
-
-it.todo('sad path not authorized')
-it.todo('sad path not found')
