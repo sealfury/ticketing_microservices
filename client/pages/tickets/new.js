d175492a8de37757fec46d044097882d3651642f
William <willspecker@gmail.com>
2021-05-24 21:44:58 +0200

    connects ticket creation form and redirects to root route on ticket creation

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,8 +1,25 @@
 import { useState } from 'react'
+import Router from 'next/router'
+import useRequest from '../../hooks/use-request'
 
 const NewTicket = () => {
   const [title, setTitle] = useState('')
   const [price, setPrice] = useState('')
+  const { makeRequest, errors } = useRequest({
+    url: '/api/tickets',
+    method: 'post',
+    body: {
+      title,
+      price,
+    },
+    onSuccess: () => Router.push('/'),
+  })
+
+  const onSubmit = event => {
+    event.preventDefault()
+
+    makeRequest()
+  }
 
   const onBlur = () => {
     const value = parseFloat(price)
@@ -17,7 +34,7 @@ const NewTicket = () => {
   return (
     <div>
       <h1>Put A Ticket Up For Sale</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className='form-group'>
           <label>Title</label>
           <input
@@ -35,6 +52,7 @@ const NewTicket = () => {
             className='form-control'
           />
         </div>
+        {errors}
         <button className='btn btn-primary'>Submit</button>
       </form>
     </div>
